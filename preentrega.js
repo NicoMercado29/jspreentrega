807dfc222656936ec73c2e2beb34637a055a802e
NicoMercado29 <nicolasmercado408@gmail.com>
2024-09-18 16:13:26 -0300

    reentrega

--- preentrega.js
+++ preentrega.js
@@ -1,49 +1,53 @@
-listaTareas = [];
+let listaTareas = [];
 
-function agregarTarea(){
-    let nuevaTarea = prompt("Ingrese una nueva tarea: "); 
-    
-    listaTareas.push(nuevaTarea); 
-    
-    console.log("Tarea agregada: " + nuevaTarea); 
-    
+function agregarTarea() {
+    let nuevaTarea = prompt("Ingrese una nueva tarea: ");
+    listaTareas.push(nuevaTarea);
+    console.log("Tarea agregada: " + nuevaTarea);
 }
-function mostrarTareas(){
-    console.log("Listas de tareas:"); //muestra este mensaje 
-    for(let i = 0; i < listaTareas.length; i++){ //Utilizamos un for para recorrer el array por completo
-        console.log(i + 1 + ". " + listaTareas[i]); //Se imprime tarea por tarea
+
+function mostrarTareas() {
+    console.log("Listas de tareas:");
+    for (let i = 0; i < listaTareas.length; i++) {
+        console.log(i + 1 + ". " + listaTareas[i]);
     }
 }
-function borrarTarea(){
-    let indiceTarea = parseInt(prompt("Ingrese el número de la tarea que desea eliminar")); //Guardamos la tarea que vamos a borrar
-    listaTareas.splice(indiceTarea-1, 1); //Eliminamos la tarea indicada
-    console.log("Tarea eliminada");
-    console.log(listaTareas); //muestra la lista final
-}function principal(){
-    let opcion; //Variable que almacena la opción seleccionada del menú
+
+function borrarTarea() {
+    let indiceTarea = parseInt(prompt("Ingrese el número de la tarea que desea eliminar"));
+    if (indiceTarea > 0 && indiceTarea <= listaTareas.length) {
+        listaTareas.splice(indiceTarea - 1, 1);
+        console.log("Tarea eliminada");
+    } else {
+        console.log("Índice no válido.");
+    }
+    console.log(listaTareas);
+}
+
+function principal() {
+    let opcion;
     do {
-        console.log("Seleccione una opción:"); //Se muestran las opciones del menú
+        console.log("Seleccione una opción:");
         console.log("1. Agregar tarea");
-        console.log("2. Mostrar tarea");
+        console.log("2. Mostrar tareas");
         console.log("3. Borrar tarea");
         console.log("0. Salir");
 
-        opcion = parseInt(prompt("Bienvenido al menú principal, por favor ingrese el número de la opción que desea ejecutar:")); 
-        switch (opcion) { 
-          
-            case1:
+        opcion = parseInt(prompt("Bienvenido al menú principal, por favor ingrese el número de la opción que desea ejecutar:"));
+        switch (opcion) {
+            case 1:
                 agregarTarea();
                 break;
-            
-            case2:
+
+            case 2:
                 mostrarTareas();
                 break;
 
-            case3:
+            case 3:
                 borrarTarea();
                 break;
 
-            case0:
+            case 0:
                 console.log("Saliendo del programa...");
                 break;
 
@@ -51,7 +55,7 @@ function borrarTarea(){
                 console.log("Opción no válida. Por favor, elige de nuevo.");
                 break;
         }
-    } while(opcion !== 0);
+    } while (opcion !== 0);
 }
 
-principal(); //Inicia el programa
+principal();
